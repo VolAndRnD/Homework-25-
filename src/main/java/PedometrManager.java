802faff6e4e0d4e244ec78b55235e448898b9e7f
VolAndRnD <volk0096@mail.ru>
2022-04-16 01:49:58 +0300

    Update 2.2.0

--- src/main/java/PedometrManager.java
+++ src/main/java/PedometrManager.java
@@ -7,14 +7,16 @@ public class PedometrManager {
 
 
     public int add(int day, int step) {
-        add.put(day, step);
+        if (add.containsKey(day)) {
+            add.put(day, add.get(day) + step);
+        } else {
+            add.put(day, step);
+        }
+
         for (Map.Entry<Integer, Integer> entry : add.entrySet()){
                 if (maxStep <= entry.getValue()){
                     maxStep = entry.getValue()+1;}
                 }
-
-
-
         return maxStep;
     }
 }
